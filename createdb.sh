6a9dc4c4ba4cf9189676cc9a30f52784f458f278
Oscar Koeroo <okoeroo@gmail.com>
2017-11-19 14:34:45 +0100

    add db script

---
+++ createdb.sh
@@ -0,0 +1,31 @@
+CREATE DATABASE domainhunter;
+CREATE USER 'domainhunter'@'localhost' IDENTIFIED BY 'domainhunter42';
+GRANT ALL PRIVILEGES ON * . * TO 'domainhunter'@'localhost';
+FLUSH PRIVILEGES;
+
+USE domainhunter;
+CREATE TABLE dns_records (
+    auto_id INTEGER NOT NULL AUTO_INCREMENT,
+    uuid VARCHAR(37) NOT NULL,
+    uuid_parent VARCHAR(37),
+    fqdn VARCHAR(2048) NOT NULL,
+    r_type VARCHAR(8) NOT NULL,
+    value VARCHAR(1000000),
+    s_dt DATETIME,
+    q_dt DATETIME,
+    r_dt DATETIME,
+    PRIMARY KEY (auto_id)
+    );
+
+CREATE TABLE no_answer(
+    auto_id INTEGER NOT NULL AUTO_INCREMENT,
+    uuid VARCHAR(37) NOT NULL,
+    uuid_parent VARCHAR(37),
+    fqdn VARCHAR(2048) NOT NULL,
+    r_type VARCHAR(8) NOT NULL,
+    reason VARCHAR(1024) NOT NULL,
+    s_dt DATETIME,
+    q_dt DATETIME,
+    r_dt DATETIME,
+    PRIMARY KEY (auto_id)
+    );
