227c05e20df8043add9fa9e4bdaef278fb4d4639
Oscar Koeroo <okoeroo@gmail.com>
2018-07-25 01:47:19 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 
-apt install python3-dnspython python3-mysqldb python3-pygraphviz python3-pip
+apt install python3-dnspython python3-pygraphviz python3-pip
 pip3 install ipwhois
 
-mysql -uroot -p < create.sql
-mkdir temp
-chown www-data:www-data temp/
+mkdir results
+mkdir db
+
+chown www-data:www-data results
+chown www-data:www-data db
+
