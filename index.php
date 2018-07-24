252e280d04d37a9f5ae2417c917934ed2a8317d6
Oscar Koeroo <okoeroo@gmail.com>
2018-07-25 00:59:51 +0200

    Code line ending clean up and new PHP code (disfuncitonal)

--- index.php
+++ index.php
@@ -1,48 +1,41 @@
 <?php
+    require_once 'globals.php';
 
-require_once 'globals.php';
+    echo '<html>';
+    echo '   <body>';
+    echo '      <form action = "/process_post.php" method = "POST">';
+    echo '         Domain: <input type="text" name="domain" autocomplete="off"> <br>';
+    echo '         Scope creep: <input type="checkbox" name="scopecreep" value="scopecreep"> <br>';
+    echo '         <input type = "submit" value = "Submit">';
+    echo '      </form>';
 
 
-echo '<html>';
-echo '   <body>';
-echo '      <form action = "/process_post.php" method = "POST">';
-echo '         Domain: <input type="text" name="domain" autocomplete="off"> <br>';
-echo '         <input type = "submit" value = "Submit">';
-echo '      </form>';
+    /* Global initializers */
+    if (!initialize()) {
+        http_response_code(500);
+        return;
+    }
 
 
-/* Global initializers */
-if (!initialize()) {
-    http_response_code(500);
-    return;
-}
+    $products = array();
+    $sql = '   SELECT fqdn, uuid_hunt '.
+           '     FROM domainhunts'.
+           ' ORDER BY s_dt';
+    $rs = $db->handle->query($sql);
 
-$db = db_connect();
-if ($db == NULL) {
-    echo 'DB connection problem';
-    return;
-}
+    echo '<table border=1>';
+    foreach($rs as $row) {
+        echo '<tr>';
 
-$products = array();
-$sql = '   SELECT fqdn, uuid_hunt, s_dt '.
-       '     FROM domainhunts'.
-       ' ORDER BY s_dt';
-$rs = $db->handle->query($sql);
+        echo '<td>' . $row['fqdn'] . '</td>';
+        echo '<td>' . '<a href="/temp/'.$row['uuid_hunt'].'.html">' . $row['s_dt'] . '</a>' . '</td>';
 
-echo '<table border=1>';
-foreach($rs as $row) {
-    echo '<tr>';
+        echo '</tr>';
+    }
+    return $products;
 
-    echo '<td>' . $row['fqdn'] . '</td>';
-    echo '<td>' . '<a href="/temp/'.$row['uuid_hunt'].'.html">' . $row['s_dt'] . '</a>' . '</td>';
+    echo '</table>';
 
-    echo '</tr>';
-}
-return $products;
-
-echo '</table>';
-
-echo '   </body>';
-echo '</html>';
-
+    echo '   </body>';
+    echo '</html>';
 ?>
