e83a6874d8b418b88bdde3587723c2aef45a84a9
Oscar Koeroo <okoeroo@gmail.com>
2018-07-31 20:12:00 +0200

    Table styling and fixed scaling by zooming out. Kudos to Willem Boogers.

--- index.php
+++ index.php
@@ -4,18 +4,41 @@
     /* header("refresh:30;url=index.html"); */
 
     echo '<html>';
-    echo '   <body>';
-    echo '   <input type="button" value="Refresh Page" onClick="window.location.reload()">';
-    echo '   <br>';
-    echo '   <br>';
+    echo '  <head>';
+    echo '  <style>';
+    echo '   table {';
+    echo '    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;';
+    echo '    border-collapse: collapse;';
+    echo '        width: 100%;';
+    echo '   }';
 
-    echo '      <form action = "/process_post.php" method = "POST">';
-    echo '         Domain: <input type="text" name="domain" autocomplete="off"> <br>';
-    echo '         Scope creep: <input type="checkbox" name="scopecreep" value="yes"> <br>';
-    echo '         (Optional) Additional FQDNs:<br><TEXTAREA NAME="otherfqdns" ROWS=10 COLS=20 ></TEXTAREA>';
-    echo '         <br>';
-    echo '         <input type = "submit" value = "Submit">';
-    echo '      </form>';
+    echo '    #domainhunts td, #domainhunts th {';
+    echo '        border: 1px solid #ddd;';
+    echo '        padding: 8px;';
+    echo '    }';
+    echo '    #domainhunts th {';
+    echo '        padding-top: 12px;';
+    echo '        padding-bottom: 12px;';
+    echo '        text-align: left;';
+    echo '        background-color: #4CAF50;';
+    echo '        color: white;';
+    echo '    }';
+    echo '    #domainhunts tr:nth-child(even){background-color: #f2f2f2;}';
+    echo '    #domainhunts tr:hover {background-color: #ddd;}';
+    echo '  </style>';
+    echo '  </head>';
+    echo '  <body>';
+    echo '  <input type="button" value="Refresh Page" onClick="window.location.reload()">';
+    echo '  <br>';
+    echo '  <br>';
+
+    echo '  <form action = "/process_post.php" method = "POST">';
+    echo '     Domain: <input type="text" name="domain" autocomplete="off"> <br>';
+    echo '     Scope creep: <input type="checkbox" name="scopecreep" value="yes"> <br>';
+    echo '     (Optional) Additional FQDNs:<br><TEXTAREA NAME="otherfqdns" ROWS=10 COLS=20 ></TEXTAREA>';
+    echo '     <br>';
+    echo '     <input type = "submit" value = "Submit">';
+    echo '  </form>';
 
 
     /* Global initializers */
@@ -29,12 +52,12 @@
            '     FROM domainhunts';
     $rs = $GLOBALS['db']->query($sql);
 
-    echo '<table border=1>';
+    echo '<table id="domainhunts">';
     echo '<tr>';
-    echo '<td>FQDN</td>';
-    echo '<td>Scope creep</td>';
-    echo '<td>Side loading</td>';
-    echo '<td>Status</td>';
+    echo '<th>FQDN</th>';
+    echo '<th>Scope creep</th>';
+    echo '<th>Side loading</th>';
+    echo '<th>Status</th>';
     echo '</tr>';
     foreach($rs as $row) {
         echo '<tr>';
