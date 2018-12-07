b95693624a279726dc2de8cfd5981b0aaec7ff60
Oscar Koeroo <okoeroo@gmail.com>
2018-12-07 23:02:54 +0100

    Added cmdline arguments parser, added /caahunter route which fetches CAA config from the DNS

--- backend-domainhunter.py
+++ backend-domainhunter.py
@@ -8,6 +8,7 @@ import uuid
 import falcon
 import requests
 import requests_cache
+import dns.resolver
 from wsgiref import simple_server
 
 URL_TLDS = 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt'
@@ -124,6 +125,50 @@ def check_fqdn_is_legit(fqdn):
     return False
 
 
+def resolve_r_type(fqdn, r_type):
+    ### DNS Resolve FQDN with resource type
+    answers = None
+    try:
+        resolver = dns.resolver.Resolver()
+        # resolver.nameservers=['8.8.8.8', '8.8.4.4', '9.9.9.9']
+        resolver.nameservers=['127.0.0.1']
+        resolver.timeout = 8
+        resolver.lifetime = 8
+        answers = resolver.query(fqdn, r_type)
+
+        results = []
+
+        for r_data in answers:
+            tup = {}
+
+            tup['fqdn'] = fqdn
+            tup['r_type'] = r_type
+            if str(r_data)[-1:] == '.':
+                tup['value'] = str(r_data)[:-1]
+            else:
+                tup['value'] = str(r_data)
+
+            results.append(tup)
+        return results
+
+    except dns.resolver.NXDOMAIN:
+        #print("Resolver warning: NXDOMAIN.", 'FQDN', fqdn, 'r_type', r_type, file=sys.stderr)
+        pass
+    except dns.resolver.NoAnswer:
+        #print("Resolver warning: SERVFAIL.", 'FQDN', fqdn, 'r_type', r_type, file=sys.stderr)
+        pass
+    except dns.exception.Timeout:
+        #print("Resolver error: Time out reached.", 'FQDN', fqdn, 'r_type', r_type, file=sys.stderr)
+        pass
+    except EOFError:
+        print("Resolver error: EOF Error.", 'FQDN', fqdn, 'r_type', r_type, file=sys.stderr)
+
+    except Exception as e:
+        print("Resolver error:", e, 'FQDN', fqdn, 'r_type', r_type, file=sys.stderr)
+
+    return None
+
+
 class DomainHunterAPI:
     CHUNK_SIZE_BYTES = 4096
 
@@ -166,21 +211,86 @@ class DomainHunterAPI:
             return
 
 
-        daemonize(domainhunter_start, j)
+class CAAHunterAPI:
+    CHUNK_SIZE_BYTES = 4096
+
+    def on_get(self, req, res):
         res.status = falcon.HTTP_200
+        res.body = "CAA hunter is Ready"
+
+    def on_post(self, req, res):
+        big_chunk = bytes()
+        while True:
+            chunk = req.stream.read(self.CHUNK_SIZE_BYTES)
+            if not chunk:
+                break
+
+            big_chunk = big_chunk + chunk
+
+        if len(big_chunk) == 0:
+            res.body = 'Error: no data provided'
+            res.status = falcon.HTTP_400
+            return
+
+        # Decode UTF-8 bytes to Unicode, and convert single quotes
+        # to double quotes to make it valid JSON
+        my_json = big_chunk.decode('utf8').replace("'", '"')
+        j = json.loads(my_json)
+
+        if j.get("domain") is None:
+            res.body = 'Error: no domain provided'
+            res.status = falcon.HTTP_400
+            return
+
+        if not check_fqdn_is_legit(j.get("domain")):
+            res.body = 'Error: not a ccTLD, gTLD or other legit TLD found'
+            res.status = falcon.HTTP_404
+            return
+
+        dns_rr_caa = resolve_r_type(j.get("domain"), 'CAA')
+        if dns_rr_caa is None:
+            res.status = falcon.HTTP_404
+        else:
+            res.body = str(dns_rr_caa)
+            res.status = falcon.HTTP_200
 
 ### Main
 if __name__ == "__main__":
+    import argparse
+
     # Init
     PATH = os.path.dirname(os.path.realpath(__file__)) + '/'
 
+    # Parser
+    parser = argparse.ArgumentParser(os.path.basename(__file__))
+    parser.add_argument("--port",
+                        help="Listening port number (default is 5000).",
+                        type=int)
+    parser.add_argument("--host",
+                        default=None,
+                        help="Listening on IP-address (default is 127.0.0.1).",
+                        type=str)
+    args = parser.parse_args()
+
+
+    # Start
     api = falcon.API()
     api.add_route('/domainhunter', DomainHunterAPI())
-
-    host = '127.0.0.1'
-    port = 5000
-    httpd = simple_server.make_server(host, port, api)
-    print("Locked and loaded for the hunt!")
-    print("Operating on", host, "port", port, "from current working dir", PATH)
     print("Loaded route: '/domainhunter'")
+    api.add_route('/caahunter', CAAHunterAPI())
+    print("Loaded route: '/caahunter'")
+
+    if args.host:
+        host = args.host
+    else:
+        host = '127.0.0.1'
+
+    if args.port:
+        port = args.port
+    else:
+        port = 5000
+
+    httpd = simple_server.make_server(host, port, api)
+    print("Operating on", host, "port", port, "from current working dir", PATH)
+    print("Locked and loaded for the hunt!")
     httpd.serve_forever()
